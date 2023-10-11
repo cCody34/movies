f11ebdc529e2b8e0ed6d05631307984e60880880
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-11 12:00:37 +0100

    import and render Person List component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Header } from "../src/components/Header";
 import { NavBar } from "../src/components/NavBar";
 import { TVList } from "./components/TVList";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <NavBar></NavBar>
       <TVList></TVList>
+      <PersonList></PersonList>
     </div>
   );
 }
