32c2f6f78a04ac27357f93bb7db2d08deef4a022
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-11 12:25:48 +0100

    create Home page

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { PersonList } from "../PersonList";
+import { TVList } from "../TVList";
+
+export const Home = () => {
+  return (
+    <>
+      <p>HOME PAGE</p>
+      <TVList />
+      <PersonList />
+    </>
+  );
+};
