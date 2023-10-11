2c46ba6c9f18676fae197417f50c65f3a8449c04
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-11 11:56:52 +0100

    import and render NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "../src/components/Header";
+import { NavBar } from "../src/components/NavBar";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <NavBar></NavBar>
     </div>
   );
 }
