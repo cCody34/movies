ea371ba98f3251c2373b7111571aa651a7967afb
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-11 11:59:26 +0100

    import and render TV List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "../src/components/Header";
 import { NavBar } from "../src/components/NavBar";
+import { TVList } from "./components/TVList";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <NavBar></NavBar>
+      <TVList></TVList>
     </div>
   );
 }
