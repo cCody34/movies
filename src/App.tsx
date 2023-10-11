47d302c6fe494ebcd7ae4031c233570622ce555f
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-11 12:26:10 +0100

    set up / path

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { Header } from "../src/components/Header";
 import { NavBar } from "../src/components/NavBar";
 import { TVList } from "./components/TVList";
 import { PersonList } from "./components/PersonList";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/pages/Home";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <NavBar></NavBar>
-      <TVList></TVList>
-      <PersonList></PersonList>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
