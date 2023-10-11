61fac1cccbce5191745f8405defe37aca97d0e68
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-11 13:50:35 +0100

    remove unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { Header } from "../src/components/Header";
 import { NavBar } from "../src/components/NavBar";
-import { TVList } from "./components/TVList";
-import { PersonList } from "./components/PersonList";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/pages/Home";
 
