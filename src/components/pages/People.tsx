bb047c2e5ea6a08b34798937e5e99e51025c0bcf
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-12 16:57:28 +0100

    create personSearch state

--- src/components/pages/People.tsx
+++ src/components/pages/People.tsx
@@ -1,11 +1,13 @@
 import { PersonList } from "../PersonList";
 import { PersonSearch } from "../PersonSearch";
+import { useState } from "react";
 
 export const People = () => {
+  const [personSearch, setPersonSearch] = useState<string>("");
   return (
     <>
-      <PersonSearch />
-      <PersonList />
+      <PersonSearch setPersonSearch={setPersonSearch} />
+      {personSearch ? <PersonList personSearch={personSearch} /> : <></>}
     </>
   );
 };
