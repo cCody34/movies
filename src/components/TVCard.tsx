ed4f075c029c8f863793781b48a039862919d8c1
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-12 15:14:31 +0100

    render name, summary and network or webchannel

--- src/components/TVCard.tsx
+++ src/components/TVCard.tsx
@@ -1,3 +1,17 @@
-export const TVCard = () => {
-    return <p>TV Card</p>
+import { Show } from "../types";
+
+interface ITVCard {
+  show: Show;
 }
+
+export const TVCard = ({ show }: ITVCard) => {
+  return (
+    <>
+      <h3>{show.name}</h3>
+      <p>Summary: </p>
+      <div dangerouslySetInnerHTML={{ __html: show.summary }}></div>
+      {show.network ? <p>Network: {show.network.name}</p> : <></>}
+      {show.webChannel ? <p>Web channel: {show.webChannel.name}</p> : <></>}
+    </>
+  );
+};
