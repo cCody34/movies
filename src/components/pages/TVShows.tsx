594eabeb4a5217757be726f0c0afb9d5992a43b9
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-12 13:13:46 +0100

    set up showSearch state

--- src/components/pages/TVShows.tsx
+++ src/components/pages/TVShows.tsx
@@ -1,9 +1,13 @@
 import { TVList } from "../TVList";
+import { useState } from "react";
+import { TVSearch } from "../TVSearch";
 
 export const TVShows = () => {
+  const [showSearch, setShowSearch] = useState<null | string>("");
   return (
     <>
-      <TVList />
+      <TVSearch setShowSearch={setShowSearch} />
+      {showSearch ? <TVList showSearch={showSearch} /> : <></>}
     </>
   );
 };
