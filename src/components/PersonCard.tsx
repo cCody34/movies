446a41f58ae1f7361b5ea88e6dec3fb93ce1e932
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-12 16:48:30 +0100

    create IPersonCard

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -1,3 +1,9 @@
-export const PersonCard = () => {
-    return <p>Person Card</p>
+import { PersonInfo } from "../types";
+
+interface IPersonCard {
+  person: PersonInfo;
 }
+
+export const PersonCard = ({ person }: IPersonCard) => {
+  return <p>Person Card</p>;
+};
