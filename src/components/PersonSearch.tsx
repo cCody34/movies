e2a8f341adb1fd3b54b49ca569ac44592c221578
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-12 16:22:33 +0100

    create PersonSearch form

---
+++ src/components/PersonSearch.tsx
@@ -0,0 +1,11 @@
+export const PersonSearch = () => {
+  return (
+    <form>
+      <label>
+        Search people:
+        <input></input>
+      </label>
+      <button>Search</button>
+    </form>
+  );
+};
