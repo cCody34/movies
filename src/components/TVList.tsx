55c765e26c7762a2a1632dc454a23ae6e4874859
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-12 13:14:12 +0100

    render TVList using showSearch and shows

--- src/components/TVList.tsx
+++ src/components/TVList.tsx
@@ -1,12 +1,31 @@
-import { TVCard } from "./TVCard"
+import { searchShows } from "../api";
+import { TVCard } from "./TVCard";
+import { useEffect, useState } from "react";
 
-export const TVList = () => {
-    return (
-    <>
-    <h3>TV LIST HERE</h3>
-    <TVCard></TVCard>
-    <TVCard></TVCard>
-    <TVCard></TVCard>
-    </>
-    )
+interface ITVList {
+  showSearch: string;
 }
+
+export const TVList = ({ showSearch }: ITVList) => {
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    searchShows(showSearch)
+      .then(({ data }) => {
+        setShows(data);
+        console.log(shows);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h3>TV LIST HERE</h3>
+      {shows.map(() => {
+        return <TVCard />;
+      })}
+    </>
+  );
+};
