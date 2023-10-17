7678d26d1865bc153fae805cfba847660f0d5ba3
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-10-17 14:10:40 +0100

    add search-people-link testid

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ export const NavBar = () => {
       <a href="/shows" data-testid="search-shows-link">
         Search TV Shows
       </a>
-      <a href="/people">Search People</a>
+      <a href="/people" data-testid="search-people-link">Search People</a>
     </>
   );
 };
