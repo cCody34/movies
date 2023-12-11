7f5bd0841d3cf9756fbebd45bfe26a15b30a2e24
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-12-11 15:46:27 +0000

    add person-card className

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -6,7 +6,7 @@ interface IPersonCard {
 
 export const PersonCard = ({ person }: IPersonCard) => {
   return (
-    <div>
+    <div className="person-card">
       <h3>{person.name}</h3>
       {person.image ? <img src={person.image.medium}></img> : <></>}
     </div>
